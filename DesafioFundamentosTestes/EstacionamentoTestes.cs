f7259a457ba47d38fd2c36e897d54f452a4283e8
Matheus Hardman <matheus.hardman@gmail.com>
2024-02-01 23:37:12 -0300

    Iniciando cobertura de testes

---
+++ DesafioFundamentosTestes/EstacionamentoTestes.cs
@@ -0,0 +1,89 @@
+using DesafioFundamentos.Models;
+
+namespace DesafioFundamentosTestes;
+
+public class UnitTest1
+{
+    [Fact]
+    public void DeveAdicionarVeiculoComPlacaValida()
+    {
+        //Arrange
+        var estacionamento = new Estacionamento(5,2);
+        var horarioEntrada = DateTime.Now;
+        var placa = "AAA-0000";
+        //Act
+        estacionamento.AdicionarVeiculo(placa);
+
+        //Assert
+        Assert.Contains(placa, estacionamento.veiculos.Keys);
+
+    }
+    [Fact]
+    public void NaoDeveAdicionarVeiculoComPlacaInvalida()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+    }
+    [Fact]
+    public void DeveValidarPlacaValida()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+
+    }
+    [Fact]
+    public void NaoDeveValidarPlacaInvalida()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+
+    }
+    [Fact]
+    public void DeveRemoverVeiculoExistenteNoEstacionamento()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+
+    }
+    [Fact]
+    public void NaoDeveRemoverVeiculoInexistenteNoEstacionamento()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+    }
+    [Fact]
+    public void DeveListarVeiculosNoEstacionamento()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+    }
+    [Fact]
+    public void NaoDeveListarVeiculosNoEstacionamento()
+    {
+        //Arrange
+        
+        //Act
+
+        //Assert
+
+    }
+    
+}
